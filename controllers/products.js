c6557a6c61ed9f901fb233961dfdb4d161b2acc5
“Rohit <webxgrafix@gmail.com>
2023-11-13 17:00:33 +0530

    Add Advance search functionality in our next api

--- controllers/products.js
+++ controllers/products.js
@@ -2,16 +2,20 @@ const product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
 
-    const { company, price } = req.query;
+    const { company, name, featured } = req.query;
     const queryobject = {}
 
-    if(company || price){
+    if (company) {
         queryobject.company = company;
-        queryobject.price = price
-        console.log(queryobject.company)
-        console.log(queryobject.price)
     }
 
+    if (featured) {
+        queryobject.featured = featured;
+    }
+    if (name) {
+        queryobject.name = { $regex: name, $options: "i" }
+    }
+    console.log(queryobject)
 
     const mydata = await product.find(queryobject)
     res.status(200).json({ mydata });
