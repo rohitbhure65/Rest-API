eac4dfa4a3586bccd3771af5f709316a6b6947f7
“Rohit <webxgrafix@gmail.com>
2023-11-13 12:42:52 +0530

    create nodejs server using express

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-    
+// GOAL : create nodejs server using express
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>{
+    res.send("server is live")
+})
+
+const start = async()=>{
+    try {
+        app.listen(PORT, ()=>{
+            console.log(`server is running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
