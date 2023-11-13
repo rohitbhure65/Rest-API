8923d8047d340229abafb89405e0bf4b9af72258
“Rohit <webxgrafix@gmail.com>
2023-11-13 17:23:39 +0530

    Add SORT functionality in rest api

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
 
-    const { company, name, featured } = req.query;
+    const { company, name, featured, sort } = req.query;
     const queryobject = {}
 
     if (company) {
@@ -13,16 +13,25 @@ const getAllProducts = async (req, res) => {
         queryobject.featured = featured;
     }
     if (name) {
-        queryobject.name = { $regex: name, $options: "i" }
+        queryobject.name = { $regex: name, $options: "i" };
+    };
+
+
+    let apiData = product.find(queryobject)
+    if (sort) {
+        let sortFix = sort.replace(",", " ")
+        apiData = apiData.sort(sortFix)
+        // console.log(apiData)
     }
+
     console.log(queryobject)
 
-    const mydata = await product.find(queryobject)
+    const mydata = await apiData;
     res.status(200).json({ mydata });
 }
 
 const getAllProductsTesting = async (req, res) => {
-    const mydata = await product.find(req.query)
+    const mydata = await product.find(req.query).sort("name -price")
     res.status(200).json({ mydata });
 }
 
