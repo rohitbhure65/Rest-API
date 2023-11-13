05d8cc484a072456898cfc1c83047d69825724ac
“Rohit <webxgrafix@gmail.com>
2023-11-13 16:35:24 +0530

    Add Filtration and Searching funtionality with query props

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,13 @@
 const product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    const mydata = await product.find({})
+    const mydata = await product.find(req.query)
     res.status(200).json({ mydata });
 }
 
 const getAllProductsTesting = async (req, res) => {
-    const mydata = await product.find({})
+    const mydata = await product.find(req.query)
     res.status(200).json({ mydata });
 }
 
-module.exports = {getAllProducts, getAllProductsTesting}
+module.exports = { getAllProducts, getAllProductsTesting }
