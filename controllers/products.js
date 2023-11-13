6986360cdd5247b2c0e8185149e5d710a275bf12
“Rohit <webxgrafix@gmail.com>
2023-11-13 15:17:44 +0530

    Testing API using postman and fixing some bugs

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,9 @@
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "Get All Producsts" });
+    res.status(200).json({ msg: "Get All Products" });
 }
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ msg: "Get All Producsts" });
+    res.status(200).json({ msg: "Get All Products Testing" });
 }
 
 module.exports = {getAllProducts, getAllProductsTesting}
